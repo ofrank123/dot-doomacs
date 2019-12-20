9ae36a8dbb17b6ce75d82df0cd699961b1b18e31
of9 <of9@st-andrews.ac.uk>
2019-12-20 17:54:51 +0000

    Added instructions to omnisharp

--- omnisharp.sh
+++ omnisharp.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#
+# Directions for omnisharp
+# Download omnisharp-mono.tar.gz
+# Copy contents to .doom.d/omnisharp-mono
+# put this script next to OmniSharp.exe
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mono $DIR/OmniSharp.exe "$@"
